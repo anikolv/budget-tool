a97cf9cc2c98ea2664b767ce1d83abc73e272cbf
Alexander Nikolov <alexander.nikolov@absolutns.com>
2019-05-02 15:15:33 +0300

    initial commit

---
+++ src/main/java/com/team1/pbt/BudgetToolApplication.java
@@ -0,0 +1,13 @@
+package com.team1.pbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetToolApplication.class, args);
+	}
+
+}
