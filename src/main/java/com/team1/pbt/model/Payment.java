cb7d3eb6c8a7096a1f09b9d50a1967a91d2db2ef
Alexander Nikolov <alexander.nikolov@absolutns.com>
2019-05-18 12:54:24 +0300

    remove boilerplate code with spring lombok, see https://projectlombok.org/setup/eclipse for installation details

--- src/main/java/com/team1/pbt/model/Payment.java
+++ src/main/java/com/team1/pbt/model/Payment.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.Data;
+
+@Data
 public class Payment {
 
 	private Double amount;
@@ -14,37 +17,4 @@ public class Payment {
 	private String budgetCategory;
 	
 	private String comment;
-
-	public Double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public String getBudgetCategory() {
-		return budgetCategory;
-	}
-
-	public void setBudgetCategory(String budgetCategory) {
-		this.budgetCategory = budgetCategory;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
 }
